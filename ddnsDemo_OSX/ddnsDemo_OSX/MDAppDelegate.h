0da1090a291e747ca284d62a312f0998663befad
will <zhangshaozhou@tclub.cn>
2014-05-06 11:39:26 +0800

    增加OSX客户端（未完成）

---
+++ ddnsDemo_OSX/ddnsDemo_OSX/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  ddnsDemo_OSX
+//
+//  Created by Will Zhang on 14-5-5.
+//  Copyright (c) 2014年 Will Zhang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ddnsDemo_OSX/ddnsDemo_OSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ddnsDemo_OSX
+//
+//  Created by Will Zhang on 14-5-5.
+//  Copyright (c) 2014年 Will Zhang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
